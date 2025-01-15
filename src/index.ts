70554a48c1eb62fbb4ae5d8a02c08f1815a738cc
pedrolucazx <pedrolucastrainee@gmail.com>
2025-01-14 22:08:26 -0300

    refactor(database connection): change the database connection file using testcontainer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import startApolloServer from '../src/server';
-import connection from './database/index';
+import { databaseConnection } from './database/index';
 
 async function start() {
   try {
-    const { url } = await startApolloServer(connection);
+    const database = await databaseConnection;
+    const { url } = await startApolloServer(database);
     console.warn(`🚀 Server ready at: ${url}`);
   } catch (error) {
     console.error(`Error starting server: ${error}`);
