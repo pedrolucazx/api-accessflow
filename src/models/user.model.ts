7e47f2c809f5d8b4819e108f2e61bdc9e3a3d62e
pedrolucazx <pedrolucastrainee@gmail.com>
2025-01-16 19:14:40 -0300

    feat(user model): create getUserByParams method

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,7 +5,15 @@ export const userModel = {
   getAllUsers: async (): Promise<User[]> => {
     return executeQuery(
       async (database) => await database<User>('usuarios').select('*'),
-      'Error fetching profiles.',
+      'Error fetching users.',
+    );
+  },
+
+  getUserByParams: async (params: Partial<User>): Promise<User | undefined> => {
+    return executeQuery(
+      async (database) =>
+        await database<User>('usuarios').where(params).first(),
+      'Error fetching user by parameters.',
     );
   },
 };
