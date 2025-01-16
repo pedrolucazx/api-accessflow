c17d99bb4d97ad915f7958a9c0f30a885c58c81e
pedrolucazx <pedrolucastrainee@gmail.com>
2025-01-16 19:15:56 -0300

    test(user model): create unit test for getUserByParams method

--- src/tests/unit/user.model.test.ts
+++ src/tests/unit/user.model.test.ts
@@ -5,13 +5,13 @@ import { executeQuery } from '../../utils/executeQuery';
 jest.setTimeout(50000);
 jest.mock('../../utils/executeQuery');
 
-describe('Profile Model Unit Tests', () => {
+describe('User Model Unit Tests', () => {
   const mockedExecuteQuery = executeQuery as jest.Mock;
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should fetch all profiles successfully', async () => {
+  it('should fetch all users successfully', async () => {
     const mockUsers: User[] = [
       {
         id: 1,
@@ -36,6 +36,23 @@ describe('Profile Model Unit Tests', () => {
     mockedExecuteQuery.mockResolvedValueOnce(mockUsers);
 
     expect(await userModel.getAllUsers()).toEqual(mockUsers);
+    expect(executeQuery).toHaveBeenCalledTimes(1);
+  });
+
+  it('should fetch a user by parameters successfully', async () => {
+    const mockUser: User = {
+      id: 1,
+      nome: 'Admin Usuário',
+      email: 'admin@exemplo.com',
+      ativo: true,
+      data_criacao: '2025-01-16T02:54:02.311Z',
+      data_update: '2025-01-16T02:54:02.311',
+      senha: 'senhaAdmin',
+    };
+
+    mockedExecuteQuery.mockResolvedValueOnce(mockUser);
+
+    expect(await userModel.getUserByParams({ id: 1 })).toEqual(mockUser);
     expect(executeQuery).toHaveBeenCalledTimes(1);
   });
 });
