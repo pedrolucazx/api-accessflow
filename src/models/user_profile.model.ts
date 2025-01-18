5abdf04ecafd40d03703302efe9a3d7d14b8c834
pedrolucazx <pedrolucastrainee@gmail.com>
2025-01-18 19:50:18 -0300

    feat(user profile model): create associate and disassociate method

---
+++ src/models/user_profile.model.ts
@@ -0,0 +1,29 @@
+import { executeQuery } from '../utils/executeQuery';
+
+export const userProfileModel = {
+  associate: async (
+    userId: number,
+    profileId: number,
+  ): Promise<number | undefined> => {
+    return executeQuery(async (database) => {
+      const [id] = await database('usuarios_perfis').insert({
+        usuario_id: userId,
+        perfil_id: profileId,
+      });
+      return id;
+    }, 'Error when trying to associate a profile with a user.');
+  },
+
+  async disassociate(userId: number, profileId: number): Promise<number> {
+    return executeQuery(
+      async (database) =>
+        await database('usuarios_perfis')
+          .where({
+            usuario_id: userId,
+            perfil_id: profileId,
+          })
+          .delete(),
+      'Error when trying to disassociate a profile with a user.',
+    );
+  },
+};
