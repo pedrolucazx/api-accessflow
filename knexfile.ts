1a735bac9c07290b755d1746e5bcb8dd342145a2
pedrolucazx <pedrolucastrainee@gmail.com>
2025-06-17 10:06:00 -0300

    feat(config): add production database configuration to knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -19,6 +19,22 @@ const config: { [key: string]: Knex.Config } = {
       directory: './src/database/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+    seeds: {
+      directory: './src/database/seeds',
+    },
+  },
   test: {
     client: 'sqlite3',
     connection: { filename: './src/database/test.db' },
